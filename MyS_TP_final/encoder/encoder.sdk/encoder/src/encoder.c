a2caff3a03484a94fb10ee7e5080fe75f1d9d595
Ignacio <imajul89@gmail.com>
2020-08-16 20:54:26 -0300

    Actualizacion SDK

---
+++ MyS_TP_final/encoder/encoder.sdk/encoder/src/encoder.c
@@ -0,0 +1,25 @@
+
+#include "xparameters.h"
+#include "xil_io.h"
+#include "encoder_ip.h"
+
+#define PRESCALER 1440   // 1440 pulsos por revolucion
+
+//====================================================
+
+int main (void) {
+
+	int posicion, direccion;
+	char *sentido[2]={"antihorario", "horario"};
+
+    xil_printf("-- Microarquitecturas y Softcores - TP Final - Ignacio Majul --\r\n");
+ 
+   while(1)
+   {
+	    posicion = ENCODER_IP_mReadReg(XPAR_ENCODER_IP_S_AXI_BASEADDR, ENCODER_IP_S_AXI_SLV_REG0_OFFSET);
+	    direccion = ENCODER_IP_mReadReg(XPAR_ENCODER_IP_S_AXI_BASEADDR, ENCODER_IP_S_AXI_SLV_REG1_OFFSET);
+	    xil_printf("Avance relativo: %d revoluciones. Sentido: %s \r\n", posicion/PRESCALER, sentido[direccion]);
+	    usleep(50000);
+   }
+ 
+}
