f531193e7b5c0dcdb9b9cafdf2613b3aa33da9a2
Ignacio <imajul89@gmail.com>
2021-04-10 18:25:04 -0300

    correcciones para aplicación practica

--- MyS_TP_final/encoder/encoder.sdk/encoder/src/encoder.c
+++ MyS_TP_final/encoder/encoder.sdk/encoder/src/encoder.c
@@ -3,13 +3,16 @@
 #include "xil_io.h"
 #include "encoder_ip.h"
 
-#define PRESCALER 1440   // 1440 pulsos por revolucion
+#define PULSOS 1440   // pulsos por rev
+#define PERIMETRO 212
+#define PRESCALER PULSOS/PERIMETRO
 
 //====================================================
 
 int main (void) {
 
-	int posicion, direccion;
+	int posicion;
+	int direccion;
 	char *sentido[2]={"antihorario", "horario"};
 
     xil_printf("-- Microarquitecturas y Softcores - TP Final - Ignacio Majul --\r\n");
@@ -18,7 +21,7 @@ int main (void) {
    {
 	    posicion = ENCODER_IP_mReadReg(XPAR_ENCODER_IP_S_AXI_BASEADDR, ENCODER_IP_S_AXI_SLV_REG0_OFFSET);
 	    direccion = ENCODER_IP_mReadReg(XPAR_ENCODER_IP_S_AXI_BASEADDR, ENCODER_IP_S_AXI_SLV_REG1_OFFSET);
-	    xil_printf("Avance relativo: %d revoluciones. Sentido: %s \r\n", posicion/PRESCALER, sentido[direccion]);
+	    xil_printf("Avance relativo: %d revoluciones. Sentido: %s \r\n", posicion, sentido[direccion]);
 	    usleep(50000);
    }
  
